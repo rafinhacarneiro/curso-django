d1f4cabc2dd41e3656b19b580b8c0f47afbacb5d
Rafael Carneiro de Moraes <rafinhacarneiro@users.noreply.github.com>
2020-11-26 15:20:29 -0300

    Initial commit

---
+++ .bat/runserver.bat
@@ -0,0 +1,2 @@
+:: Inicia o servidor HTTP
+python manage.py runserver 127.0.0.1:80
---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index( request):
+    
+    context = {
+        "curso": "Programação Web com Django Framework"
+    }
+
+    return render( request, 'index.html', context )
+
+def contato( request):
+    return render( request, 'contato.html' )
